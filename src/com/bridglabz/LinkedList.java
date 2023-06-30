dc071084a01a451340674c9fbdf72732fa2ad34c
nehanarta <nehabaswanthrao@gmail.com>
2023-06-30 19:49:12 +0530

    Added:create simple linkedList

---
+++ src/com/bridglabz/LinkedList.java
@@ -0,0 +1,27 @@
+package com.bridglabz;
+
+public class LinkedList {
+    Node head;
+    Node tail;
+    public void add(int data){
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+    
+    public void display() {
+        Node currentNode=head;
+        if (currentNode==null){
+            System.out.println("Linked list is empty");
+        }
+        while (currentNode !=null){
+            System.out.print(currentNode.data+" -> ");
+            currentNode=currentNode.next;
+        }
+        System.out.println("null");
+    }
+}
--- src/com/bridglabz/LinkedListMain.java
+++ src/com/bridglabz/LinkedListMain.java
@@ -2,6 +2,12 @@ package com.bridglabz;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        System.out.println("welcome to the data Structure");
+       // System.out.println("welcome to the data Structure");
+        LinkedList Ls=new LinkedList();
+        Ls.add(30);
+        Ls.add(56);
+        Ls.add(70);
+        Ls.display();
+
     }
 }
